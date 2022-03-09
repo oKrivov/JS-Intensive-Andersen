180b1f353558bfff26ea820974af5814e7d5a915
oKrivov <krivovoleg23@gmail.com>
2022-03-09 17:10:11 +0300

    changed tabs to spaces

--- JS_Core/hw_1.js
+++ JS_Core/hw_1.js
@@ -1,29 +1,28 @@
 function conversionOfNumberSystem() {
-	const value = prompt().trim();
-	const base = prompt().trim();
-	const valueIsNanOrEmpty = isNaN(value) || isNaN(base) || value === '';
-	const notNumberSystem = base < 2 || base > 32;
+  const value = prompt().trim();
+  const base = prompt().trim();
+  const valueIsNanOrEmpty = isNaN(value) || isNaN(base) || value === '';
+  const notNumberSystem = base < 2 || base > 32;
 
-	if (valueIsNanOrEmpty || notNumberSystem) {
-		return console.log('Некорректный ввод!');
-	}
+  if (valueIsNanOrEmpty || notNumberSystem) {
+    return console.log('Некорректный ввод!');
+  }
 
-	return console.log((+value).toString(base));
+  return console.log((+value).toString(base));
 }
 
 // conversionOfNumberSystem();
 
 
 function sumAndQuotientOfTwoNumbers() {
-	const value1 = +prompt().trim();
-	const value2 = +prompt().trim();
-	const valueIsNanOrEmpty = value1 === '' || value2 === '' || isNaN(value1) || isNaN(value2);
+  const value1 = +prompt().trim();
+  const value2 = +prompt().trim();
+  const valueIsNanOrEmpty = value1 === '' || value2 === '' || isNaN(value1) || isNaN(value2); 
+  if (valueIsNanOrEmpty || value2 === 0) {
+    return console.log('Некорректный ввод!');
+  }
 
-	if (valueIsNanOrEmpty || value2 === 0) {
-	  return console.log('Некорректный ввод!');
-	} 
-	
-	return console.log(`Ответ: ${value1 + value2}, ${(value1 / value2)}`);
+  return console.log(`Ответ: ${value1 + value2}, ${(value1 / value2)}`);
 }
 
 // sumAndQuotientOfTwoNumbers();
