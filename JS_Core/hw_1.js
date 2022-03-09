ceeeb34b79d205783045ed3c8b425d241650401a
oKrivov <krivovoleg23@gmail.com>
2022-03-09 15:11:37 +0300

    changes with code style guidelines

--- JS_Core/hw_1.js
+++ JS_Core/hw_1.js
@@ -1,18 +1,25 @@
-function conversionOfNumberSystem (value, base) {
-	value = prompt().trim();
-	base = prompt().trim();
-	if(isNaN(value) || isNaN(base) || base < 2 || base > 32 || value === ''){
+function conversionOfNumberSystem() {
+	const value = prompt().trim();
+	const base = prompt().trim();
+	const valueIsNanOrEmpty = isNaN(value) || isNaN(base) || value === '';
+	const notNumberSystem = base < 2 || base > 32;
+
+	if (valueIsNanOrEmpty || notNumberSystem) {
 		return console.log('Некорректный ввод!');
 	}
 
 	return console.log((+value).toString(base));
 }
+
 // conversionOfNumberSystem();
 
-function sumAndQuotientOfTwoNumbers(value1, value2) {
-	value1 = +prompt().trim();
-	value2 = +prompt().trim();
-	if(value1 === '' || value2 === '' || value2 === 0 ||isNaN(value1) || isNaN(value2)){
+
+function sumAndQuotientOfTwoNumbers() {
+	const value1 = +prompt().trim();
+	const value2 = +prompt().trim();
+	const valueIsNanOrEmpty = value1 === '' || value2 === '' || isNaN(value1) || isNaN(value2);
+
+	if (valueIsNanOrEmpty || value2 === 0) {
 		return console.log('Некорректный ввод!');
 	} 
 	
