ba20abf59f25694cc7537ed3460d38605f04cde6
oKrivov <krivovoleg23@gmail.com>
2022-03-18 12:38:29 +0300

    added index and array to function arguments

--- JS_Core/hw_3.js
+++ JS_Core/hw_3.js
@@ -1,14 +1,17 @@
-
 const arr = [1, 3, 45, 33, 55, 213];
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
 Array.prototype.myFilter = function(callbackFunc) {
   const newArray = [];
-
+  const array = this;
+  let index = 0;
+  
   for (let item of this) {
-    if (callbackFunc(item)) {
+    if (callbackFunc(item, index, array)) {
       newArray.push(item);
     }
+
+    index++;
   }
 
   return newArray;
@@ -16,17 +19,21 @@ Array.prototype.myFilter = function(callbackFunc) {
 
 
 Array.prototype.myReduce = function(callbackFunc, initialValue) {
+  const array = this;
   let result = null;
+  let index = 0;
 
   if (initialValue) {
     result = initialValue;
 
     this.forEach((item) => {
-      result = callbackFunc(result, item);
+      result = callbackFunc(result, item, index, array);
+      index++;
     });
   } else {
     this.forEach((item) => {
-      result = callbackFunc(result, item);
+      result = callbackFunc(result, item, index, array);
+      index++;
     });
   }
 
